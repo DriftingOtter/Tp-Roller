ca4f3487d0c6ea99fb3e834f7f8bc2a318d16675
Daksh <dakshkaul7790@gmail.com>
2024-02-03 19:04:25 +0530

    Added README.md, LICENSE, and basic transpiler arg parsing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tp Roller
+
+> A Toml -> Html transpiler for quick webpage production.
+
+W.I.P
+
+# Acknowledgements
+- For .toml parser
+[toml11](https://github.com/ToruNiina/toml11?tab=readme-ov-file)
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include </home/daksh/Documents/TP_Roller/toml11/toml.hpp>
+#include <regex>
+
+int checkfor_toml (std::string file_path);
+
+int main(int argc, char* argv[]){
+
+    // Check for any args 
+    if (argc == 1){
+        std::cout << "TP_Roller: $ TP_Roller <name_of_toml_file>" << std::endl;
+        return -1;
+    }
+
+    // Check for .toml file
+    std::string transpile_file = argv[1];
+    if (checkfor_toml(transpile_file) == -1){
+        std::cout << "TP_Roller: file given is not a .toml file." << std::endl;
+        return -1;
+    }
+
+    // TODO: Start Making The Transpile Code
+
+    return 0;
+}
+
+int checkfor_toml (std::string file_path){
+    std::regex toml_pattern("\\.toml$", std::regex_constants::icase);
+
+    if (!std::regex_search(file_path, toml_pattern)){
+        return -1;
+    }
+
+    return 0;
+}
